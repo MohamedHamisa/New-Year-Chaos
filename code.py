0cc2003c578321be3ce5fed561d9bd35bd203234
Mohamed Hamisa <108889205+MohamedHamisa@users.noreply.github.com>
2023-01-29 23:16:20 +0200

    Create code.py

---
+++ code.py
@@ -0,0 +1,11 @@
+def minimumBribes(q):
+    bribe = 0
+    for i in range(len(q) - 1, -1, -1):
+        if q[i] - (i + 1) > 2:
+            print("Too chaotic")
+            return
+        for j in range(max(0, q[i] - 2), i):
+            if q[j] > q[i]:
+                bribe += 1
+    print(bribe)
+
